7f52bc28ef3163b0152b19a4a1fcb9cc224a0601
matthew-johnson <mpjohnson1414@gmail.com>
2014-04-16 17:58:46 -0500

    Solved for the point of intersection between the line from image to receiver and the plane PQR.

--- Matt Trace.py
+++ Matt Trace.py
@@ -22,38 +22,10 @@ n = normalv/np.linalg.norm(normalv)
 print 'normalized = ' + str(n)
 
 #Source position
-source = np.array([7,4,4])
-receiver = np.array([-5,-5,20])
+source = np.array([3,4,4])
+receiver = np.array([5,5,5])
 source_vector = np.array(source-Q)
 print 'Source position = ' + str(source)
-
-#Mult-dimensional array of the points P, Q, and R multiplied element-wise by the normalized normal vector.
-'''
-A = np.array([[(n[0]*P[0]),(n[1]*P[1]),(n[2]*P[2])],
-            [(n[0]*Q[0]),(n[1]*Q[1]),(n[2]*Q[2])],
-            [(n[0]*R[0]),(n[1]*R[1]),(n[2]*R[2])]])
-B = np.array([-1,-1,-1])
-
-print A
-print B
-'''
-
-#A least square estimation of the solution of the system of equations. Solving the form Ax = B where x = [1/D, 1/D, 1/D]
-'''
-least_square = np.linalg.lstsq(A, B)
-print least_square
-'''
-#Parsing the result of the least square estimation to determine the plane constant D.
-'''
-def find_D(lst_sq):
-    for row in lst_sq:
-        for entry in row:
-            if entry != 0:
-                D = 1/entry
-                return D
-
-D = find_D(least_square)
-'''
 
 #Dot product of source position and normalized normal vector.
 source_distance = np.dot(n, source_vector)
@@ -63,23 +35,16 @@ print 'Distance from source to plane = ' + str(source_distance)
 image = source - (2*source_distance*n)
 print 'Image position = ' + str(image)
 
-print receiver - image
+image_v = receiver - image
+print 'Distance from image to receiver = ' + str(image_v)
 print PQ
 print PR
 
-#If the determinant below is = 0, then the three vectors are linearly dependent meaning the line does not intersect the plane and the reflection is not valid.
-check_reflection = np.linalg.det(np.array([receiver - image, PQ, PR]))
-print 'Determinant = ' + str(check_reflection)
-if check_reflection != 0:
-    print 'Valid reflection.'
-else:
-    print 'Invalid reflection.'
-
 #Solving for the intersection point of the line from image to reciever and the plane PQR. http://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection
 A = np.array([
-    [image[0]-receiver[0], PQ[0], PR[0]],
-    [image[1]-receiver[1], PQ[1], PR[1]],
-    [image[2]-receiver[2], PQ[2], PR[2]]
+    [image_v[0], PQ[0], PR[0]],
+    [image_v[1], PQ[1], PR[1]],
+    [image_v[2], PQ[2], PR[2]]
 ])
 
 B = np.array([
@@ -93,5 +58,13 @@ intersect_mag = x[0]
 print intersect_mag
 
 #Intersection point in form Ia + (Ib - Ia)t where Ia is the image position and Ib is the receiver position.
-intersect = image + (receiver - image)*intersect_mag
+intersect = image + (image_v)*intersect_mag
 print 'Reflection point = ' + str(intersect)
+
+#If the determinant below is = 0, then the three vectors are linearly dependent meaning the line does not intersect the plane and the reflection is not valid.
+check_reflection = np.dot(intersect - Q, n)
+print 'Distance = ' + str(check_reflection)
+if check_reflection == 0:
+    print 'Valid reflection.'
+else:
+    print 'Invalid reflection. Point does not intersect the wall.'
